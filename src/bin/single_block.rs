ef059ed48b9d61717ecf4cbeefe214b1b90be5dc
Dmitry Shuranov <dvshur@gmail.com>
2021-03-01 17:51:45 +0300

    single_block parameterized height

--- src/bin/single_block.rs
+++ src/bin/single_block.rs
@@ -5,10 +5,11 @@ use std::env::var;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = var("URL").unwrap_or("http://localhost:6881".to_owned());
+    let height = var("HEIGHT").unwrap_or("1".to_owned()).parse::<i32>()?;
     let mut client = BlockchainUpdatesApiClient::connect(url).await?;
 
     let request = tonic::Request::new(GetBlockUpdateRequest {
-        height: 150000
+        height: height
     });
 
     let response = client.get_block_update(request).await?;
