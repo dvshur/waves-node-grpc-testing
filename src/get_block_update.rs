02cb09bf5aaef441ebfb91635d8fe7c6145d67e6
Dmitry Shuranov <dvshur@gmail.com>
2023-04-13 22:26:58 +0300

    dependencies update

--- src/get_block_update.rs
+++ src/get_block_update.rs
@@ -1,20 +1,30 @@
+use std::env::var;
 use waves_protobuf_schemas::waves::events::grpc::blockchain_updates_api_client::BlockchainUpdatesApiClient;
 use waves_protobuf_schemas::waves::events::grpc::GetBlockUpdateRequest;
-use std::env::var;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = var("URL").unwrap_or("http://localhost:6881".to_owned());
-    let height = var("HEIGHT").unwrap_or("1".to_owned()).parse::<i32>()?;
+    let url = var("URL")
+        .map(|s| {
+            println!("URL {}", s);
+            s
+        })
+        .unwrap_or("http://localhost:6881".to_owned());
+
+    let height = var("HEIGHT")
+        .map(|s| {
+            println!("HEIGHT {}", s);
+            s.parse::<i32>().expect("FROM_HEIGHT must be integer")
+        })
+        .unwrap_or(1);
+
     let mut client = BlockchainUpdatesApiClient::connect(url).await?;
 
-    let request = tonic::Request::new(GetBlockUpdateRequest {
-        height: height
-    });
+    let request = GetBlockUpdateRequest { height };
 
     let response = client.get_block_update(request).await?;
 
     println!("{:?}", response);
-    
+
     Ok(())
 }
---
+++ src/subscribe.rs
@@ -0,0 +1,68 @@
+use std::env::var;
+use std::time::Duration;
+use waves_protobuf_schemas::waves::events::grpc::blockchain_updates_api_client::BlockchainUpdatesApiClient;
+use waves_protobuf_schemas::waves::events::grpc::SubscribeRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = var("URL")
+        .map(|s| {
+            println!("URL {}", s);
+            s
+        })
+        .unwrap_or("http://localhost:6881".to_owned());
+
+    let from_height = var("FROM_HEIGHT")
+        .map(|s| {
+            println!("FROM_HEIGHT {}", s);
+            s.parse::<i32>().expect("FROM_HEIGHT must be integer")
+        })
+        .unwrap_or(1);
+
+    let to_height = var("TO_HEIGHT")
+        .map(|s| {
+            println!("TO_HEIGHT {}", s);
+            s.parse::<i32>().expect("TO_HEIGHT must be integer")
+        })
+        .unwrap_or(0);
+
+    let silent = var("SILENT")
+        .map(|s| {
+            println!("SILENT {}", s);
+            s.parse::<bool>().expect("SILENT must be boolean")
+        })
+        .unwrap_or(false);
+
+    let throttle_millis = var("THROTTLE_MILLIS")
+        .map(|s| {
+            println!("THROTTLE_MILLIS {}", s);
+            s.parse::<u64>()
+                .expect("THROTTLE_MILLIS must be unsigned long")
+        })
+        .unwrap_or(0);
+
+    let mut client = BlockchainUpdatesApiClient::connect(url).await?;
+
+    let request = SubscribeRequest {
+        from_height,
+        to_height,
+    };
+
+    let mut stream = client.subscribe(request).await?.into_inner();
+
+    while let Some(msg) = stream.message().await? {
+        let upd = msg.update.unwrap();
+
+        if !silent {
+            println!("{:?}", &upd.height);
+        }
+
+        if throttle_millis > 0 {
+            tokio::time::sleep(Duration::from_millis(throttle_millis)).await;
+        }
+    }
+
+    println!("Finished streaming");
+
+    Ok(())
+}
